a4ca733a448e0ed520d8a47474bf4d6179a62f72
ahmedbellaaj10 <ahmed-bellaaj@outlook.com>
2022-08-04 15:47:08 -0400

    fix other sh files and clean structure

--- RL4E2E/scripts/galaxy/train_agent_2.0.sh
+++ RL4E2E/scripts/galaxy/train_agent_2.0.sh
@@ -3,12 +3,12 @@
 cd ../../../
 WORK_DIR=$(pwd)
 echo $WORK_DIR
-myvirtualenv/bin/activate
+
 cd RL4E2E
 
 python train_pdqn.py \
-    --episodes=3 \
-    --epsilon_steps=2000 \
+    --episodes=2000 \
+    --epsilon_steps=10000 \
     --action=train \
     --model=galaxy \
     --version=2.0
