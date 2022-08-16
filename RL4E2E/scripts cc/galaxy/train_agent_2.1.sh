88bbade21393de33cdfb93db64236dab9194f1a6
ahmedbellaaj10 <ahmed-bellaaj@outlook.com>
2022-08-16 17:56:32 -0400

    add compute canada scripts

---
+++ RL4E2E/scripts cc/galaxy/train_agent_2.1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --account=def-foutsekh
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=168:00:00
+
+
+module load python/3.8.10
+virtualenv --no-download ENV
+source ENV/bin/activate
+
+pip install --no-index -r requirementscc.txt
+
+cd ../../../
+WORK_DIR=$(pwd)
+echo $WORK_DIR
+
+cd RL4E2E
+
+python train_pdqn.py \
+    --episodes=2000 \
+    --epsilon_steps=2000 \
+    --action=train \
+    --model=galaxy \
+    --version=2.1 \
+    --num_selected_actions=1
---
+++ RL4E2E/scripts cc/pptod/train_agent_2.0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ../../../
+WORK_DIR=$(pwd)
+echo $WORK_DIR
+
+cd RL4E2E
+
+python train_pdqn.py \
+    --episodes=2000 \
+    --epsilon_steps=2000 \
+    --action=train \
+    --model=pptod \
+    --version=2.0 \
+    --num_selected_actions=1
+    
