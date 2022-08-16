cb5dedd66b5f1715286a3091a67cd5f2161d9f39
ahmedbellaaj10 <ahmed-bellaaj@outlook.com>
2022-08-16 18:01:44 -0400

    add compute canada scripts

--- RL4E2E/scripts cc/pptod/evaluate_agent2.1.sh
+++ RL4E2E/scripts cc/pptod/evaluate_agent2.1.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+#SBATCH --account=def-foutsekh
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=168:00:00
 
+
+module load python/3.8.10
+virtualenv --no-download ENV
+source ENV/bin/activate
+
+pip install --no-index -r requirementscc.txt
 cd ../../../
 WORK_DIR=$(pwd)
 echo $WORK_DIR
